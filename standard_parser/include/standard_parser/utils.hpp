2c61707297a37f0afc9b828f18fe5c13a0983c41
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2025-01-04 13:52:29 +0100

    Removed standard_parser

--- standard_parser/include/standard_parser/utils.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-#include <vector>
-
-namespace wisevision::utils {
-  std::string convertBinaryToHexString(const std::vector<uint8_t>& bytes);
-  std::vector<uint8_t> convertHexStringToBinary(const std::string& str);
-} // namespace wisevision::utils
