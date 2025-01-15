b8fc0b0240f6168c25b307c54b186cadd3a0720a
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2025-01-15 21:57:46 +0100

    Add check license CI action and script which checks licenses (#7)
    * Add check license CI action and script which checks licenses
    
    * Update licence checker
    
    * Add dependeny to standard_parser because it is always needed
    
    * Moved standard_parser to the top of repository
    
    * Add LICENSE file and license content to each file
    
    * Change license check to ready Github Action solution (#8)
    
    * Change license check to ready Github Action solution
    
    * Decrease version for action

--- chirpstack_docker/import_lorawan_devices.sh
+++ chirpstack_docker/import_lorawan_devices.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+#
+#  Copyright (C) 2025 wisevision
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
 
 docker compose run --rm --entrypoint sh --user root chirpstack -c '\
     apk add --no-cache git && \
