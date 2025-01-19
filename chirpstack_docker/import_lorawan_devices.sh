6c276c27547be031c549e30cf497cffbdda102ca
Bartlomiej Styczen <styczen.bartlomiej@gmail.com>
2025-01-19 20:29:04 +0100

    Add updated license header

--- chirpstack_docker/import_lorawan_devices.sh
+++ chirpstack_docker/import_lorawan_devices.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+#
+#  Copyright (C) 2025 wisevision
+#
+#  SPDX-License-Identifier: MPL-2.0
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#
+
 
 docker compose run --rm --entrypoint sh --user root chirpstack -c '\
     apk add --no-cache git && \
